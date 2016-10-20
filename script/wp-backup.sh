c4677993b6bdcf37fd3680dc57add15530f249d8
Alejandro Mangione <alejandro.ferraiolo@brasilct.com>
2016-10-20 11:16:03 -0200

    remove loop

--- script/wp-backup.sh
+++ script/wp-backup.sh
@@ -3,30 +3,26 @@
 echo "\n"
 echo "========================================================="
 echo "======= Script to Backup WordPress Sites ================"
-echo "=========================================================\n"
+echo "========================================================="
 
 now=$(date +'%Y%m%d')
 
-echo "- Backup database\n"
+echo "- Backup database"
 read -p "Database name: " name_db
 read -p "Enter user: " user_db
 read -p "Enter host: " host_db
 
 mysqldump -h $host_db -u $user_db -p -R --opt $name_db > $now'-backup-dump-wp-brasilct.sql'
 
-echo "\n"
+echo ""
 echo "- Database file: "$now'-backup-dump-wp-brasilct.sql'
-echo "\n"
+echo ""
 
-echo "- Backup WP Files\n"
+echo "- Backup WP Files"
 
-path="-"
 
-while [ ! -d $path ]
-do
-        echo "Directory $path not exists."
-        read -p "Enter WP folder name: /var/www/" folder_wp
-        path="/var/www/"$folder_wp
-done
-
+echo "- Enter directory path:"
+read -p "Enter WP folder name: /var/www/" folder_wp
+path="/var/www/"$folder_wp
+        
 zip -r $now"-backup-wp-files.zip" $path"/"
